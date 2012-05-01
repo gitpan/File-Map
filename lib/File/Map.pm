a3ceb55e51d7f213e74564c1b492f9cd41febb96
Leon Timmermans <fawaka@gmail.com>
2012-05-01 20:38:57 +0000

    Import of LEONT/File-Map-0.50 from CPAN.
    gitpan-cpan-distribution: File-Map
    gitpan-cpan-version:      0.50
    gitpan-cpan-path:         LEONT/File-Map-0.50.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

--- lib/File/Map.pm
+++ lib/File/Map.pm
@@ -1,6 +1,6 @@
 package File::Map;
 {
-  $File::Map::VERSION = '0.49';
+  $File::Map::VERSION = '0.50';
 }
 
 # This software is copyright (c) 2008, 2009, 2010 by Leon Timmermans <leont@cpan.org>.
@@ -131,7 +131,7 @@ File::Map - Memory mapping made simple and safe.
 
 =head1 VERSION
 
-version 0.49
+version 0.50
 
 =head1 SYNOPSIS
 
